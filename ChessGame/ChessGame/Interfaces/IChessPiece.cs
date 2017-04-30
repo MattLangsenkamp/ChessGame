71545b64244c7fc43ca422f32430be0a83f15d35
MattLangsenkamp <mattlangsenkamp@yahoo.com>
2017-04-29 20:46:03 -0400

    Added IBoard, IBoardManager and IChessPiece interfaces

---
+++ ChessGame/ChessGame/Interfaces/IChessPiece.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame.Interfaces
+{
+	interface IChessPiece
+	{
+
+		bool move(int x, int y);
+	}
+}
